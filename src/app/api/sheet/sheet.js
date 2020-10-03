1797eec4a863f0e4c8289749cae6bab2b4cd01cf
Gabriel Luis da Silva <gabriel.luis.cinza@gmail.com>
2020-10-03 20:33:46 -0300

    Feature/docker watcher (#1)
    * add docker with ln and nodemon
    
    * Add readme

--- src/app/api/sheet/sheet.js
+++ src/app/api/sheet/sheet.js
@@ -2,4 +2,8 @@ module.exports = function buildSheetRoutes (app) {
   app.get('/sheet', (req, res) => {
     res.json({ route: 'sheet' })
   })
+
+  app.get('/', (req, res) => {
+    res.json({})
+  })
 }
